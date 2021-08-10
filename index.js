813517f48f0a7a5dde809cf692aa5e9449b52723
xiaohao <19929238507@163.com>
2021-08-11 02:01:41 +0800

    实现判赢

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var Player;
     Player["X"] = "x";
     Player["O"] = "o";
 })(Player || (Player = {}));
+// 判赢数组
+var winsArr = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6] // 斜
+];
 // 获取所有单元格列表
 var cells = document.querySelectorAll('.cell');
 var gameBord = document.querySelector('#bord');
@@ -19,9 +25,32 @@ function clickCell(event) {
     // 给当前点击事件的单元格添加类名x
     var target = event.target;
     target.classList.add(currentPlayer);
+    // 判断是否获胜
+    var isWin = checkWin(currentPlayer);
+    if (isWin) {
+        console.log('赢了');
+    }
     // 切换玩家
     currentPlayer = currentPlayer === Player.X ? Player.O : Player.X;
     // 切换玩家提示
     gameBord.classList.remove(Player.O, Player.X);
     gameBord.classList.add(currentPlayer);
+}
+// 判赢函数
+function checkWin(player) {
+    var isWin = winsArr.some(function (item) {
+        var cellIndex1 = item[0];
+        var cellIndex2 = item[1];
+        var cellIndex3 = item[2];
+        var cell1 = cells[cellIndex1];
+        var cell2 = cells[cellIndex2];
+        var cell3 = cells[cellIndex3];
+        if (cell1.classList.contains(player) &&
+            cell2.classList.contains(player) &&
+            cell3.classList.contains(player)) {
+            return true;
+        }
+        return false;
+    });
+    return isWin;
 }
--- index.ts
+++ index.ts
@@ -3,13 +3,18 @@ enum Player {
     X = 'x',
     O = 'o'
 }
-
+// 判赢数组
+const winsArr = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  // 横
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // 竖
+    [0, 4, 8], [2, 4, 6]  // 斜
+]
 // 获取所有单元格列表
 let cells = document.querySelectorAll('.cell')
 
 let gameBord = document.querySelector('#bord')
 // 玩家
-let currentPlayer: Player = Player.X
+let currentPlayer = Player.X
 
 
 
@@ -24,10 +29,41 @@ function clickCell(event: MouseEvent) {
     // 给当前点击事件的单元格添加类名x
     let target = event.target as HTMLDivElement
     target.classList.add(currentPlayer)
+
+    // 判断是否获胜
+    let isWin = checkWin(currentPlayer)
+    if (isWin) {
+        console.log('赢了');
+        
+    }
+
     // 切换玩家
     currentPlayer = currentPlayer === Player.X ? Player.O : Player.X
 
     // 切换玩家提示
     gameBord.classList.remove(Player.O, Player.X)
     gameBord.classList.add(currentPlayer)
+}
+
+// 判赢函数
+function checkWin(player: Player): boolean {
+    let isWin = winsArr.some(function (item) {
+        let cellIndex1 = item[0]
+        let cellIndex2 = item[1]
+        let cellIndex3 = item[2]
+
+        let cell1 = cells[cellIndex1]
+        let cell2 = cells[cellIndex2]
+        let cell3 = cells[cellIndex3]
+
+        if (
+            cell1.classList.contains(player) &&
+            cell2.classList.contains(player) &&
+            cell3.classList.contains(player) 
+        ) {
+            return true
+        } 
+        return false
+    })
+    return isWin
 }
